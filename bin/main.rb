fdc0ccbb25d50e051dfa5b1bfdb530a73b25641a
shoirata <shoira.shakirovna@gmail.com>
2021-04-06 14:05:23 +0500

    def board

--- bin/main.rb
+++ bin/main.rb
@@ -11,6 +11,18 @@ player2_name = gets.chomp.capitalize
 puts
 puts "#{player2_name} your symbol  is \"o\""
 puts
-puts "#{player1_name} and #{player2_name}  get ready to play TicToc Game.."
+puts "#{player1_name} and #{player2_name}  get ready to play TicTac Game.."
 puts
 puts
+
+
+board = [1,2,3,4,5,6,7,8,9]
+def display_board (board)
+  puts '-------------'
+  puts "| #{board[0]} | #{board[1]} | #{board[2]} |"
+  puts '-------------'
+  puts "| #{board[3]} | #{board[4]} | #{board[5]} |"
+   puts '-------------'
+  puts "| #{board[6]} | #{board[7]} | #{board[8]} |"
+   puts '-------------'
+end
