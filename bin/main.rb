4ed1b06d663c71309420c671ba7bdb6de36a40a9
shoirata <shoira.shakirovna@gmail.com>
2021-04-06 14:03:39 +0500

    game introduction & getting user names

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,16 @@
 #!/usr/bin/env ruby
+#getting name of player 1
+puts "What is the name of Player 1?"
+player1_name = gets.chomp.capitalize
+puts 
+puts "Hello #{player1_name}, your symbol  is \"x\""
+#getting name of player 2
+puts
+puts "What is the name of player 2?"
+player2_name = gets.chomp.capitalize
+puts
+puts "#{player2_name} your symbol  is \"o\""
+puts
+puts "#{player1_name} and #{player2_name}  get ready to play TicToc Game.."
+puts
+puts
