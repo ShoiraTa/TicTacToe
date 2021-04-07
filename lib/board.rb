b7dd48da076eb72d4d2f5bc721d9323c9fbdda1b
shoirata <shoira.shakirovna@gmail.com>
2021-04-07 11:25:01 +0500

    board class

---
+++ lib/board.rb
@@ -0,0 +1,15 @@
+class Board
+# attr_accessor :arr
+#      def initialize(arr)
+#           @arr = arr
+#      end
+def display_board(board)
+  puts '-------------'
+  puts "| #{board[0]} | #{board[1]} | #{board[2]} |"
+  puts '-------------'
+  puts "| #{board[3]} | #{board[4]} | #{board[5]} |"
+  puts '-------------'
+  puts "| #{board[6]} | #{board[7]} | #{board[8]} |"
+  puts '-------------'
+end
+end
