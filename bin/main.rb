a0be18d508c346f63e54ccca5d1b90f810ab7ee7
shoirata <shoira.shakirovna@gmail.com>
2021-04-07 11:24:14 +0500

    game intro

--- bin/main.rb
+++ bin/main.rb
@@ -1,92 +1,92 @@
 #!/usr/bin/env ruby
+require_relative "../lib/player"
+require_relative "../lib/board"
 
-def name_valid?(name)
-  true if name.length >= 3 && !name.nil?
-end
-
+puts "Welcome to TicTacToe game"
+puts "Welcome player 1"
+player1 = Player.new
 puts 'What is the name of Player 1?'
 player1_name = gets.chomp.capitalize
-until name_valid?(player1_name)
+until player1.name_valid?(player1_name)
   puts 'Please enter a valid name, it should have at least 3 characters'
   player1_name = gets.chomp.capitalize
 end
-puts "Hello #{player1_name}, your symbol  is \"x\""
+puts "Hello #{player1_name}, your symbol  is \"X\""
+puts
+puts
+
+puts "Welcome player 2"
+player2 = Player.new
 puts 'What is the name of player 2?'
 player2_name = gets.chomp.capitalize
-until name_valid?(player2_name)
+until player2.name_valid?(player2_name)
   puts 'Please enter a valid name, it should have at least 3 characters'
   player2_name = gets.chomp.capitalize
 end
-puts "#{player2_name} your symbol  is \"o\""
+puts "#{player2_name} your symbol  is \"O\""
 puts "#{player1_name} and #{player2_name}  get ready to play TicTac Game.."
 
-board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-def display_board(board)
-  puts '-------------'
-  puts "| #{board[0]} | #{board[1]} | #{board[2]} |"
-  puts '-------------'
-  puts "| #{board[3]} | #{board[4]} | #{board[5]} |"
-  puts '-------------'
-  puts "| #{board[6]} | #{board[7]} | #{board[8]} |"
-  puts '-------------'
-end
 
-puts display_board(board)
+board = Board.new() 
+board.display_board([1, 2, 3, 4, 5, 6, 7, 8, 9])
+puts " #{player1_name} it is your turn now, select your \"X\" position"
+player1
 
-def user_input_is_valid?(user_input)
-  true if user_input.between?(0, 9) && !user_input.nil?
-end
 
-def user_input_to_i(user_input)
-  user_input.to_i - 1
-end
 
-game_over = false
 
-until game_over
 
-  puts "#{player1_name} it is your turn now, select your \"X\" position"
-  user_input = gets.chomp
-  user_input = user_input_to_i(user_input)
 
-  until user_input_is_valid?(user_input) == true
-    puts 'Enter an available board number'
-    user_input = gets.chomp
-    user_input = user_input_to_i(user_input)
-    puts user_input_is_valid?(user_input)
-  end
+# def user_input_to_i(user_input)
+#   user_input.to_i - 1
+# end
 
-  board[user_input] = 'X'
-  p display_board(board)
+# game_over = false
 
-  break if board.none?(Integer)
+# until game_over
 
-  puts "#{player2_name} it is your turn now, select your \"O\" position"
-  user_input = gets.chomp
-  user_input = user_input_to_i(user_input)
+#  
+#   user_input = gets.chomp
+#   user_input = user_input_to_i(user_input)
+#  puts "#{player1} it is your turn now, select your \"X\" position"
+#   until user_input_is_valid?(user_input) == true
+#     puts 'Enter an available board number'
+#     user_input = gets.chomp
+#     user_input = user_input_to_i(user_input)
+#     puts user_input_is_valid?(user_input)
+#   end
 
-  until user_input_is_valid?(user_input) == true
-    puts 'Enter an available board number'
-    user_input = gets.chomp
-    user_input = user_input_to_i(user_input)
-    puts user_input_is_valid?(user_input)
-  end
+#   board[user_input] = 'X'
+#   p display_board(board)
 
-  board[user_input] = 'O'
-  display_board(board)
+#   break if board.none?(Integer)
 
-  game_over = true if board.none?(Integer)
+#   puts "#{player2} it is your turn now, select your \"O\" position"
+#   user_input = gets.chomp
+#   user_input = user_input_to_i(user_input)
 
-end
+#   until user_input_is_valid?(user_input) == true
+#     puts 'Enter an available board number'
+#     user_input = gets.chomp
+#     user_input = user_input_to_i(user_input)
+#     puts user_input_is_valid?(user_input)
+#   end
 
-def winner(board, player1_name, player2_name)
-  if board.count('X') > board.count('O')
-    puts "#{player1_name}, you WIN the game"
-  elsif board.count('X') < board.count('O')
-    puts "#{player2_name}, you WIN the game"
-  else
-    puts 'This game is a tie'
-  end
-end
+#   board[user_input] = 'O'
+#   display_board(board)
 
-winner(board, player1_name, player2_name)
+#   game_over = true if board.none?(Integer)
+
+# end
+
+# def winner(board, player1, player2)
+#   if board.count('X') > board.count('O')
+#     puts "#{player1}, you WIN the game"
+#   elsif board.count('X') < board.count('O')
+#     puts "#{player2}, you WIN the game"
+#   else
+#     puts 'This game is a tie'
+#   end
+# end
+
+# winner(board, player1, player2)
