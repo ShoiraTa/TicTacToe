49d2d5675cfb7c3af2a6add4b05cef49070b5ed5
shoirata <shoira.shakirovna@gmail.com>
2021-04-07 18:24:24 +0500

    fix rubocop err

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,9 @@
-include Enumerable
 class Board
-attr_accessor :win_combination
-    def initialize()
-@win_combination= [
+  include Enumerable
+  attr_accessor :win_combination
+
+  def initialize()
+    @win_combination = [
       [0, 1, 2],
       [3, 4, 5],
       [6, 7, 8],
@@ -11,26 +12,23 @@ attr_accessor :win_combination
       [2, 5, 8],
       [2, 4, 6],
       [0, 4, 8]
-     ]
-end
- def win?(board)
+    ]
+  end
+
+  def win?(board)
     @win_combination.any? do |combination|
       combination.all? { |idx| board[idx] == 'X' } || combination.all? { |idx| board[idx] == 'O' }
     end
   end
 
-
-
-def position_valid?(board, index)
-if board[index] == "X" || board[index] == "O"
-     puts 
-puts 'This position is taken, choose another position'
-puts
-return false
-else 
-return true
-end
-end
-
-
+  def position_valid?(board, index)
+    if board[index] == 'X' || board[index] == 'O'
+      puts
+      puts 'This position is taken, choose another position'
+      puts
+      false
+    else
+      true
+    end
+  end
 end
