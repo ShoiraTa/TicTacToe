250f226e44a99473254a061ce1fcfdf25bde0b75
shoirata <shoira.shakirovna@gmail.com>
2021-04-08 12:35:26 +0500

    fix rubocop err

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,9 @@
 class Player
   def name_valid?(name)
-    true if  !name.nil? && name.length >= 3
+    true if !name.nil? && name.length >= 3
   end
 
   def player_input_valid?(player_input)
-    true if  !player_input.nil? &&  player_input.between?(0, 9) 
+    true if !player_input.nil? && player_input.between?(0, 9)
   end
 end
